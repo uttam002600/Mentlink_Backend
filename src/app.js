db80a2f038255894f98f58cf63299324ca0250c3
Uttam <uttam002600@gmail.com>
2025-04-03 15:22:16 +0530

    Cors setting

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
