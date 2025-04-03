beb6c50b594b7a9604c6ff6d2231b40bcd7dde4c
Uttam <uttam002600@gmail.com>
2025-04-03 13:02:44 +0530

    fic

--- src/app.js
+++ src/app.js
@@ -11,9 +11,9 @@ app.use(
   })
 );
 
-app.use(express.json({ limit: "16kb" }));
+app.use(express.json({ limit: "100kb" }));
 // app.use(express.urlencoded({ extended: true }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "100kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
